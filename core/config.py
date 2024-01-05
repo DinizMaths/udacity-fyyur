cd77bd366debc9e9d87785b92219d91d8234d5bc
Matheus <dinizmaths@gmail.com>
2024-01-05 13:04:50 -0300

    added venues endpoints

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ class Config:
     DEBUG = True
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:pgsql@localhost:5432/fyyur"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 APP_HOST = "0.0.0.0"
 APP_PORT = 3000
-
--- models/show.py
+++ models/show.py
@@ -2,10 +2,13 @@ from core.database import db
 
 
 class Show(db.Model):
-    __tablename__ = 'show'
+    __tablename__ = "show"
 
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
-    pass
+
+    venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"))
+    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"))
+
+    def __repr__(self):
+        return f"<Show {self.id} {self.start_time}>"
